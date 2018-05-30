5508435a3de20ca461c8a50db02d4e164ee34a90
Anthony J. DeMeglio <22527727+ademeglio@users.noreply.github.com>
2018-05-29 20:31:49 -0400

    update .gitignore per spec; added VirtualPetShelter()

---
+++ src/main/java/org/wecancodeit/virtual_pet_shelter/VirtualPetShelterApp.java
@@ -0,0 +1,22 @@
+package org.wecancodeit.virtual_pet_shelter;
+
+public class VirtualPetShelterApp {
+
+	/*
+	 * WeCanCodeIT Module Project 3
+	 * by Anthony J. DeMeglio
+	 * 5/30/2018
+	 * 
+	 * Virtual Pet Shelter
+	 * Volunteer at the pet shelter and take care of the the pets!
+	 * 
+	 * https://github.com/ademeglio/virtual-pet-shelter
+	 * 
+	 */
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
